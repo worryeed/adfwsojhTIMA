89d630ba09a0cf9d387f0526f8f23cf3db25f783
worryeed <94265342+worryeed@users.noreply.github.com>
2024-04-21 18:32:52 +0300

    Add files via upload

---
+++ MyCalcLbTests/MyCalcTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MyCalcLbTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Sum_10and20_30returned()
+        {
+            //arrange
+            int x = 10;
+            int y = 20;
+            int expected = 30;
+            //act
+            MyCalcLb c = new MyCalc();
+            int actual = c.Sum(x, y);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ MyCalcLbTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyCalcLbTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("MyCalcLbTests")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed315bb3-478a-45a8-a05d-9ad88cee1d52")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
